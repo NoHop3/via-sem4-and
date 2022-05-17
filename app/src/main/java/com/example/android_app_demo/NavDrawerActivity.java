290de66ffd8117e2f7f98caec07b4291499756dc
NoHop3 <u_sux@abv.bg>
2022-05-17 15:48:56 +0200

    Final commit to Alex - next Pull Request to main

--- app/src/main/java/com/example/android_app_demo/NavDrawerActivity.java
+++ app/src/main/java/com/example/android_app_demo/NavDrawerActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.android_app_demo.viewModels.NavDrawerViewModel;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -23,15 +26,17 @@ public class NavDrawerActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityNavDrawerBinding binding;
     private NavDrawerViewModel viewModel;
+    Button logOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //this.logOutBtn = findViewById(R.id.sign_out_btn);
+
+
         viewModel = new ViewModelProvider(this).get(NavDrawerViewModel.class);
-
         checkIfSignedIn();
-
-
         binding = ActivityNavDrawerBinding.inflate(getLayoutInflater());
        // getSupportActionBar().setTitle("FoodyRecipe");
 
@@ -59,16 +64,31 @@ public class NavDrawerActivity extends AppCompatActivity {
 
                 setSupportActionBar(binding.appBarNavDrawer.toolbar);
                 DrawerLayout drawer = binding.drawerLayout;
+//                logOutBtn = drawer.findViewById(R.id.logOut);
+                System.out.println(logOutBtn);
+//                logOutBtn = drawer.findViewById(R.id.sign_out_btn);
+//                logOutBtn.setOnClickListener(view -> signOut());
+
                 NavigationView navigationView = binding.navView;
                 // Passing each menu ID as a set of Ids because each
                 // menu should be considered as top level destinations.
                 mAppBarConfiguration = new AppBarConfiguration.Builder(
-                        R.id.nav_home, R.id.nav_productList, R.id.nav_shoppingList, R.id.nav_previousShoppings)
+                        R.id.nav_home, R.id.nav_productList
+                        //, R.id.nav_shoppingList, R.id.nav_previousShoppings
+                        )
                         .setOpenableLayout(drawer)
                         .build();
                 NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_nav_drawer);
                 NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
                 NavigationUI.setupWithNavController(navigationView, navController);
+
+                navigationView.getMenu().findItem(R.id.logOut).setOnMenuItemClickListener(menuItem -> {
+                    if (drawer.isDrawerOpen(GravityCompat.START)) {
+                        drawer.closeDrawer(GravityCompat.START);
+                    }
+                    signOut();
+                    return true;
+                });
             } else
                 startLoginActivity();
         });
